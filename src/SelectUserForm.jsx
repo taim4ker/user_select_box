a53dc3308400e517fcb19698da7ed1cde593f316
t.sakaedani <65404350+taim4ker@users.noreply.github.com>
2024-08-23 07:35:38 +0000

    chapter1 コンポーネントを作成

--- src/SelectUserForm.jsx
+++ src/SelectUserForm.jsx
@@ -3,64 +3,41 @@ import { user_data } from "./user_data";
 
 const SelectUserForm = () => {
   const [keyword, setKeyword] = useState("");
-  const [showLists, setShowLists] = useState(false);
   const [filteredUsers, setFilteredUsers] = useState([]);
-  const [timer, setTimer] = useState(null);
 
   const fetchUsers = () => {
+    // 初回マウント時はスキップするため
     if (keyword === "") {
       return;
     }
 
+    // ユーザーデータのうち、IDがkeywordを含むものを絞り込む
     const filtered_data = user_data.filter((d) =>
       d.id.toString().includes(keyword)
     );
 
     setFilteredUsers(filtered_data);
-    setShowLists(true);
   };
 
   useEffect(() => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-
-    // 500ミリ秒まってから実行
-    const newTimer = setTimeout(() => {
-      fetchUsers();
-    }, 500);
-
-    setTimer(newTimer);
-
-    return () => clearTimeout(newTimer);
+    fetchUsers();
   }, [keyword]);
 
   return (
     <>
-      <div className="c-forms__content is-md">
-        <input
-          type="integer"
-          value={keyword}
-          className="forms__title"
-          placeholder="ユーザーIDを入力"
-          onChange={(e) => setKeyword(e.target.value)}
-        />
-        {showLists && (
-          // @todo 見た目を整形する
-          <select
-            className="select-field"
-            size="5"
-            id="user_id"
-            name="reservation[user_id]"
-          >
-            {filteredUsers.map((user, index) => (
-              <option key={index} value={user.id}>
-                {user.id} {user.name}
-              </option>
-            ))}
-          </select>
-        )}
-      </div>
+      <input
+        type="integer"
+        value={keyword}
+        placeholder="ユーザーIDを入力"
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+      <select size="5">
+        {filteredUsers.map((user, index) => (
+          <option key={index} value={user.id}>
+            {user.id} {user.name}
+          </option>
+        ))}
+      </select>
     </>
   );
 };
