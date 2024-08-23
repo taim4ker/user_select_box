7f1c2a27c7ab33443a1dc1bf5f93f95560c6f749
t.sakaedani <65404350+taim4ker@users.noreply.github.com>
2024-08-23 08:15:14 +0000

    chapter3 表示ロジックをカスタムhookに分割

---
+++ src/SelectUserForm.hook.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { user_data } from "./user_data";
+
+export function useSelectUserForm({ keyword }) {
+  const [showLists, setShowLists] = useState(false);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  const fetchUsers = () => {
+    // 初回マウント時はスキップするため
+    if (keyword === "") {
+      return;
+    }
+
+    // ユーザーデータのうち、IDがkeywordを含むものを絞り込む
+    const filtered_data = user_data.filter((d) =>
+      d.id.toString().includes(keyword)
+    );
+
+    setFilteredUsers(filtered_data);
+    setShowLists(true);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [keyword]);
+
+  return {
+    showLists,
+    filteredUsers,
+  },
+}
